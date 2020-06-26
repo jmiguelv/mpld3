79ae64ce38f3c8cea076f66a8114cc82e45fa279
Miguel Vieira <jmvieira@pm.me>
2020-06-26 17:24:42 +0100

    🐛 Fix: JS path.

--- mpld3/urls.py
+++ mpld3/urls.py
@@ -9,19 +9,23 @@ import warnings
 
 from . import __path__, __version__
 
-__all__ = ["D3_URL", "MPLD3_URL", "MPLD3MIN_URL",
-           "D3_LOCAL", "MPLD3_LOCAL", "MPLD3MIN_LOCAL"]
+__all__ = [
+    "D3_URL",
+    "MPLD3_URL",
+    "MPLD3MIN_URL",
+    "D3_LOCAL",
+    "MPLD3_LOCAL",
+    "MPLD3MIN_LOCAL",
+]
 
-WWW_JS_DIR = "https://jmiguelv.github.io/mpld3/mpld3/js"
+WWW_JS_DIR = "https://jmiguelv.github.io/mpld3/mpld3/js/"
 D3_URL = WWW_JS_DIR + "d3.v5.min.js"
 # TODO: Add this to GitHub pages
-D3_URL = 'https://d3js.org/d3.v5.js'
+D3_URL = "https://d3js.org/d3.v5.js"
 MPLD3_URL = WWW_JS_DIR + "mpld3.v{0}.js".format(__version__)
 MPLD3MIN_URL = WWW_JS_DIR + "mpld3.v{0}.min.js".format(__version__)
 
 LOCAL_JS_DIR = os.path.join(__path__[0], "js")
 D3_LOCAL = os.path.join(LOCAL_JS_DIR, "d3.v5.min.js")
-MPLD3_LOCAL = os.path.join(LOCAL_JS_DIR,
-                           "mpld3.v{0}.js".format(__version__))
-MPLD3MIN_LOCAL = os.path.join(LOCAL_JS_DIR,
-                              "mpld3.v{0}.min.js".format(__version__))
+MPLD3_LOCAL = os.path.join(LOCAL_JS_DIR, "mpld3.v{0}.js".format(__version__))
+MPLD3MIN_LOCAL = os.path.join(LOCAL_JS_DIR, "mpld3.v{0}.min.js".format(__version__))
