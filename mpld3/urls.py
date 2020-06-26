9776182de94df3285e4be297d653b7ffb59f85e1
Miguel Vieira <jmvieira@pm.me>
2020-06-26 17:18:13 +0100

    🐛 Fix: JS path.

--- mpld3/urls.py
+++ mpld3/urls.py
@@ -5,14 +5,15 @@ URLs and filepaths for the mpld3 javascript libraries
 """
 
 import os
-from . import __path__, __version__
 import warnings
+
+from . import __path__, __version__
 
 __all__ = ["D3_URL", "MPLD3_URL", "MPLD3MIN_URL",
            "D3_LOCAL", "MPLD3_LOCAL", "MPLD3MIN_LOCAL"]
 
-WWW_JS_DIR = "https://mpld3.github.io/js/"
-D3_URL = WWW_JS_DIR + "d3.v3.min.js"
+WWW_JS_DIR = "https://jmiguelv.github.io/mpld3/mpld3/js"
+D3_URL = WWW_JS_DIR + "d3.v5.min.js"
 # TODO: Add this to GitHub pages
 D3_URL = 'https://d3js.org/d3.v5.js'
 MPLD3_URL = WWW_JS_DIR + "mpld3.v{0}.js".format(__version__)
